00fbf3b2cb7a4d141042f5a25def449bc20f9f4d
E-Mello <e.mello.dev@gmail.com>
2024-01-26 10:17:02 -0400

    Update attendance record UI and styles (Add dialog html component)

--- js/attendance-record.js
+++ js/attendance-record.js
@@ -1,20 +1,5 @@
-function registrarPresenca() {
-  // Obtém o nome do aluno
-  var nomeAluno = document.getElementById("nome-aluno").value;
+const dialog = document.querySelector("dialog");
 
-  // Verifica se o nome do aluno não está vazio
-  if (nomeAluno.trim() === "") {
-    alert("Por favor, insira o nome do aluno.");
-    return;
-  }
-
-  // Cria um elemento de lista para o aluno
-  var novoItem = document.createElement("li");
-  novoItem.textContent = nomeAluno;
-
-  // Adiciona o aluno à lista de alunos presentes
-  document.getElementById("alunos-presentes").appendChild(novoItem);
-
-  // Limpa o campo do nome do aluno
-  document.getElementById("nome-aluno").value = "";
+function closeDialog() {
+  document.querySelector("dialog").close();
 }
